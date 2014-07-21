23f168d0c441abb3cea85a277d8c531932f87b2e
Sebastien Tricaud <sebastien@honeynet.org>
2014-07-21 05:33:19 +0200

    Better gcrypt finder script

--- cmake/Findgcrypt.cmake
+++ cmake/Findgcrypt.cmake
@@ -1,2 +1,9 @@
-SET(GCRYPT_LIBRARY "/opt/local/lib/libgcrypt.dylib")
-SET(GCRYPT_INCLUDE "/opt/local/include")
+find_path(GCRYPT_INCLUDE_DIR gcrypt.h)
+find_library(GCRYPT_LIBRARY gcrypt)
+
+set(GCRYPT_LIBRARIES ${GCRYPT_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Gcrypt DEFAULT_MSG GCRYPT_LIBRARIES GCRYPT_INCLUDE_DIR)
+
+mark_as_advanced(GCRYPT_LIBRARIES GCRYPT_INCLUDE_DIR)
